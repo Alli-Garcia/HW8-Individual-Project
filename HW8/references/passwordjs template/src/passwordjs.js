5212a838b38f3e7b0f1580119a3043512dc30187
Alli <alligarcia2021@gmail.com>
2024-05-03 13:06:06 -0400

    commiting node modules

--- HW8/references/passwordjs template/src/passwordjs.js
+++ HW8/references/passwordjs template/src/passwordjs.js
@@ -1,32 +1,77 @@
-'use strict'
-const fs = require('fs');
-const util = require('./utility')
+'use strict';
+const mongoose = require('mongoose');
+const util = require('./utility');
+
+mongoose.connect('mongodb+srv://garciaa12:H96KSSfEpwDvlWk7@ase285cluster.drz3zaw.mongodb.net/app_data?retryWrites=true&w=majority');
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+db.on('disconnected', () => {
+    console.log('Disconnected from MongoDB');
+});
+
+process.on('SIGINT', () => {
+    db.close(() => {
+        console.log('MongoDB connection closed');
+        process.exit(0);
+    });
+});
+
+const passwordSchema = new mongoose.Schema({
+    email: String,
+    hashedPassword: String
+});
+
+const passwordModel = mongoose.model('password', passwordSchema);
+
+async function uploadToMongoDB(credentials) {
+    try {
+        await passwordModel.insertMany(credentials);
+        console.error('Upload to MongoDB completed.');
+        
+    } catch (err) {
+        console.error('Error uploading to MongoDB:', err);
+    } finally {
+        mongoose.disconnect();
+    }
+    };
 
 function passwordjs() {
-    if (process.argv.length != 5) return 'false';
+    if (process.argv.length !== 5) return 'false';
 
-    var filename = process.argv[2]
-    var email = process.argv[3]
-    var password = process.argv[4]
+    const filename = process.argv[2];
+    const email = process.argv[3];
+    const password = process.argv[4];
 
-    const credentials = util.readFile(filename);
+    const isAuthenticate = util.authenticate(email, password);
 
-    if (!credentials) return 'false';
-
-    const hashedPassword = util.hash(password);
-
-    for (const line of credentials) {
-        const [fileEmail, filePassword] = line.split(':');
-        if (fileEmail === email && util.hash(filePassword) === hashedPassword) {
-            return 'true';
-        }
+    if (isAuthenticate) {
+        return true;
+    }else {
+        return false;
     }
 
-    return 'false';
 }
 
 if (require.main === module) {
-    console.log(passwordjs()) // print out true or false
+    console.log(passwordjs()); // Print out true or false
+
+    // Upload encrypted credentials to MongoDB
+    const credentials = util.readFile(process.argv[2]);
+    const encryptedCredentials = credentials.map(line => {
+        const [email, password] = line.split(':');
+        return { email, hashedPassword: util.hash(password) };
+    });
+
+    uploadToMongoDB(encryptedCredentials);
 }
 
-module.exports = {passwordjs};
+module.exports = { passwordjs };
--- HW8/references/passwordjs template/tests/makepassword.test.js
+++ HW8/references/passwordjs template/tests/makepassword.test.js
@@ -5,9 +5,8 @@
 // 3. Make sure the contents of password.enc.txt has correct contents.
 // For unit tests, you don't have to have a large input in the beginning.
 // Start with smallest input, and add more contents in the input
-
-const p = require('../src/makepassword');
-const u = require('../src/utility');
+const mongoose = require('mongoose');
+const { makepassword, passwordModel } = require('../src/makepassword');
 const fs = require('fs');
 
 /*
@@ -21,19 +20,48 @@ test('Check toHash(): if the email:password is converted into email:hashPassword
 */
 
 describe("makepassword should create file", () => {
-    test('',() => {
-        const fileName = './tests/passwordtest.txt'
-        const encFileName = './tests/passwordtest.enc.txt'
+
+    let hashedPasswords;
+
+    beforeAll(async () => {
+        // Connect to MongoDB
+        await mongoose.connect('mongodb+srv://garciaa12:H96KSSfEpwDvlWk7@ase285cluster.drz3zaw.mongodb.net/app_data?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        // Query MongoDB to get hashed passwords
+        hashedPasswords = await passwordModel.find({}, 'email hashedPassword');
+    });
+
+    afterAll(async () => {
+        // Disconnect from MongoDB after all tests are done
+        await mongoose.disconnect();
+    });
+    
+    test('Check if password.enc.txt is created with correct contents', async () => {
+        const fileName = 'HW8/references/passwordjs template/tests/passwordtest.txt'
+        const encFileName = 'HW8/references/passwordjs template/src/password.enc.txt'
 
         // 1. Make sure password.enc.txt does not exist before running the function.
-        ???
+        expect(fs.existsSync(encFileName)).toBe(false);
         
-        p.makepassword(fileName, encFileName)
+        await makepassword(fileName, encFileName)
 
         // 2. Make sure password.enc.txt does exist after running the function.
-        ???
+        expect(fs.existsSync(encFileName)).toBe(true);
 
         // 3. Make sure the contents of password.enc.txt has correct contents.
-        ???
+        const expectedContent = hashedPasswords.map(({email,hashedPassword}) => ({
+            email,
+            hashedPassword
+        }));
+
+        const fileContents = fs.readFileSync(encFileName, 'utf-8');
+        const expectedJson = JSON.stringify(expectedContent, null, 2);
+        const actualJson = JSON.stringify(JSON.parse(fileContents), null, 2);
+
+        expect(actualJson).toBe(expectedJson);
+
+        });
     })
-})
